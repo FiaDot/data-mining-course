12c283dc86309bfc32ea205b6cc5f95e51a60f39
fiadot <fiadot@gmail.com>
2014-04-26 19:27:04 +0900

    저장소 로그기반 마이닝 준비

---
+++ httpd/load.py
@@ -0,0 +1,13 @@
+# How to access XML? see also : http://sweeper.egloos.com/3045370
+from xml.etree.ElementTree import parse
+tree = parse("httpd_log_simple.xml")
+log_root = tree.getroot()
+
+for parent in tree.getiterator("logentry"):
+    for child in parent:
+        print(child)    
+        # print(child.text)
+        # print(child.find("path").text)
+        child.attrib["paths"]
+
+
